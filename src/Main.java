be5b9d10b885eacff374e40d887ee2165ae76483
hdhaliwa <hdhaliwa@asu.edu>
2024-03-12 09:54:56 -0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int N = 10;
+
+        Fibonacci(N);
+    }
+
+    private static void Fibonacci(int n) {
+        int n1 = 0;
+        int n2 = 1;
+
+        for(int i = 0; i<=n;i++) {
+            System.out.print(n1 + " ");
+            int n3 = n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
